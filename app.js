d8ea7f75f6f8031204bd4f4ea866f99fc04e456a
khavi <vib2105566@student.ctu.edu.vn>
2025-03-23 00:53:23 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -8,6 +8,17 @@ app.use(express.json());
 
 app.use("/api/books", booksRouter);
 
+app.use((req, res, next) => {
+    return next(new ApiError (404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+     message: error.message || "Internal Server Error",
+    });
+});
+
+
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to book application." });
 });
