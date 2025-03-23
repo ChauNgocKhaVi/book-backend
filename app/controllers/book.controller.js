8ee910675abcdd4258ff146bcbdff19577c178d3
khavi <vib2105566@student.ctu.edu.vn>
2025-03-23 14:56:05 +0700

    Cai dat cac handler truy xuat CSDL

--- app/controllers/book.controller.js
+++ app/controllers/book.controller.js
@@ -1,26 +1,91 @@
-exports.create = (req, res) => {
-    res.send({ message: "create handler" });
+const BookService = require("../services/book.service");
+const MongoDB = require("../utils/mongodb.util");
+const ApiError = require("../api-error");
 
+exports.create = async (req, res, next) => {
+  if (!req.body?.TenSach) {
+    return next(new ApiError(400, "Tên sách không được để trống"));
+  }
+
+  try {
+    const bookService = new BookService(MongoDB.client);
+    const document = await bookService.create(req.body);
+    return res.send(document);
+  } catch (error) {
+    return next(
+      new ApiError(500, "Đã xảy ra lỗi khi tạo sách")
+    );
+  }
 };
 
-exports.findALL = (req, res) => {
-    res.send({ message: "findAll handler" });
+exports.findAll = async (req, res, next) => {
+  let documents = [];
 
+  try {
+    const bookService = new BookService(MongoDB.client);
+    const { TenSach } = req.query;
+    if (TenSach) {
+      documents = await bookService.findByName(TenSach);
+    } else {
+      documents = await bookService.find({});
+    }
+  } catch (error) {
+    return next(new ApiError(500, "Đã xảy ra lỗi khi truy xuất danh sách sách"));
+  }
+
+  return res.send(documents);
 };
 
-exports.findOne = (req, res) => {
-    res.send({ message: "findOne handler" });
+exports.findOne = async (req, res, next) => {
+  try {
+    const bookService = new BookService(MongoDB.client);
+    const document = await bookService.findById(req.params.id);
+    if (!document) {
+      return next(new ApiError(404, "Không tìm thấy sách"));
+    }
+    return res.send(document);
+  } catch (error) {
+    return next(new ApiError(500, `Lỗi khi truy xuất sách với id=${req.params.id}`));
+  }
 };
 
-exports.update = (req, res) => {
-    res.send({ message: "update handler" });
+exports.update = async (req, res, next) => {
+  if (Object.keys(req.body).length === 0) {
+    return next(new ApiError(400, "Dữ liệu cập nhật không được để trống"));
+  }
+
+  try {
+    const bookService = new BookService(MongoDB.client);
+    const document = await bookService.update(req.params.id, req.body);
+    if (!document) {
+      return next(new ApiError(404, "Không tìm thấy sách"));
+    }
+    return res.send({ message: "Sách đã được cập nhật thành công" });
+  } catch (error) {
+    return next(new ApiError(500, `Lỗi khi cập nhật sách với id=${req.params.id}`));
+  }
 };
 
-exports.delete = (req, res) => {
-    res.send({ message: "delete handler" });
+exports.delete = async (req, res, next) => {
+  try {
+    const bookService = new BookService(MongoDB.client);
+    const document = await bookService.delete(req.params.id);
+    if (!document) {
+      return next(new ApiError(404, "Không tìm thấy sách"));
+    }
+    return res.send({ message: "Sách đã được xóa thành công" });
+  } catch (error) {
+    return next(new ApiError(500, `Không thể xóa sách với id=${req.params.id}`));
+  }
 };
 
-exports.deleteAll = (req, res) => {
-    res.send({ message: "deleteAll handler" });
+exports.deleteAll = async (_req, res, next) => {
+  try {
+    const bookService = new BookService(MongoDB.client);
+    const deletedCount = await bookService.deleteAll();
+    return res.send({ message: `${deletedCount} sách đã được xóa thành công` });
+  } catch (error) {
+    return next(new ApiError(500, "Đã xảy ra lỗi khi xóa tất cả sách"));
+  }
 };
 
