ae8b2f40cb360a0ff6f897e974c564bca03018b5
khavi <vib2105566@student.ctu.edu.vn>
2025-03-23 17:36:58 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ const ApiError = require("./app/api-error");
 const app = express();
 
 const booksRouter = require("./app/routes/book.route");
+const readersRouter = require("./app/routes/reader.route");
+const publishersRouter = require("./app/routes/publisher.route");
+const staffsRouter = require("./app/routes/staff.route");
+const borrowsRouter = require("./app/routes/borrow.route");
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/books", booksRouter);
+app.use("/api/readers", readersRouter);
+app.use("/api/publishers", publishersRouter);
+app.use("/api/staffs", staffsRouter);
+app.use("/api/borrows", borrowsRouter);
 
 app.use((req, res, next) => {
     return next(new ApiError (404, "Resource not found"));
