32684afcabe11adbdea288742514c872904388c8
khavi <vib2105566@student.ctu.edu.vn>
2025-03-23 17:37:47 +0700

    Cai dat cac handler truy xuat CSDL

---
+++ app/routes/staff.route.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const staffs = require('../controllers/staff.controller');
+
+const router = express.Router();
+
+router.route("/")
+    .get(staffs.findAll)
+    .post(staffs.create)
+    .delete(staffs.deleteAll);
+
+router.route("/:id")
+    .get(staffs.findOne)
+    .put(staffs.update)
+    .delete(staffs.delete);
+
+module.exports = router;
