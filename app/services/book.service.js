1f0205f27e06743672f865a7a0e7a529a4b6eace
khavi <vib2105566@student.ctu.edu.vn>
2025-03-23 14:56:16 +0700

    Cai dat cac handler truy xuat CSDL

---
+++ app/services/book.service.js
@@ -0,0 +1,78 @@
+const { ObjectId, ReturnDocument } = require("mongodb");
+const ApiError = require("../api-error");
+const MongoDB = require("../utils/mongodb.util");
+
+class BookService {
+    constructor(client) {
+        this.Book = client.db().collection("books");
+    }
+    extractBookData(payload) {
+        const book = {
+            TenSach: payload.TenSach,
+            DonGia: payload.DonGia,
+            SoQuyen: payload.SoQuyen,
+            NamXuatBan: payload.NamXuatBan,
+            MaNXB: payload.MaNXB,
+            TacGia: payload.TacGia,
+        };
+        Object.keys(book).forEach(
+            (key) => book[key] === undefined && delete book[key]
+        );
+        return book;
+    }
+
+    async create(payload) {
+        const book = this.extractBookData(payload);
+        const result = await this.Book.findOneAndUpdate(
+            book,
+            { $set: book },
+            { ReturnDocument: "after", upsert: true }
+        );
+        return result;
+    }
+
+    async find(filter) {
+        const cursor = await this.Book.find(filter);
+        return await cursor.toArray();
+    }
+
+    async findByName(name) {
+        return await this.find({
+            TenSach: { $regex: new RegExp(name, "i") },
+        });
+    }
+
+    async findById(id) {
+        return await this.Book.findOne({
+            _id: ObjectId.isValid(id) ? new ObjectId(id) : null,
+        });
+    }
+
+    async update(id, payload) {
+        const filter = { _id: ObjectId.isValid(id) ? new ObjectId(id) : null};
+        const update = this.extractBookData(payload);
+        const result = await this.Book.findOneAndUpdate(
+            filter,
+            { $set: update },
+            { ReturnDocument: "after" }
+        );
+        return result;
+    }
+
+    async delete(id) {
+        const result = await this.Book.findOneAndDelete({
+            _id: ObjectId.isValid(id) ? new ObjectId(id) : null,
+        });
+        return result;
+    }
+
+    async deleteAll() {
+        const result = await this.Book.deleteMany({});
+        return result.deletedCount;
+    }
+}
+
+module.exports = BookService;
+
+
+module.exports = BookService;
