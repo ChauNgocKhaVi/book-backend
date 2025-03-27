02aed2313433d7eae4c34ae25506ff42fd8bf764
khavi <vib2105566@student.ctu.edu.vn>
2025-03-27 18:55:46 +0700

    Update book.route.js

--- app/routes/book.route.js
+++ app/routes/book.route.js
@@ -8,10 +8,15 @@ router.route("/")
     .get(books.findAll)
     .post(books.create)
     .delete(books.deleteAll);
-
+    
 router.route("/:id")
     .get(books.findOne)
     .put(books.update)
     .delete(books.delete);
+
+// cập nhật số quyển bị trừ    
+router.route("/borrow/:id").put(books.borrowBook); 
+
+router.patch("/:id/returnBook", books.returnBook);
 
 module.exports = router;
